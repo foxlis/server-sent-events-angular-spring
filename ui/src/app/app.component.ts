65a0fd92f4cf0ff2c9c64739f237dc3f1717b5e7
Patryk Kozak <pkozak@corelogic.com>
2018-10-31 19:33:40 +0100

    Add missing files

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ui';
+  eventData;
+
+  connectEventSource() {
+    const eventSource = new EventSource('http://localhost:8080/progress');
+    eventSource.addEventListener('progressEvent', message => {
+      this.eventData = (<any>message).data;
+    })
+  }
 }
